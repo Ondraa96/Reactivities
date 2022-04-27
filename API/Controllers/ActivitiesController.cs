199859575315a546cdbcac32984d70defc6104c8
Ondřej Vít <ondra.vit@getfix.cz>
2022-04-27 18:27:30 +0200

    init

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Activity>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }    
+        
+        [HttpGet("{id}")]
+        public async Task<Activity> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
