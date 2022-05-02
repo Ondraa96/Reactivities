2667d633e1946505246f7a6e2c478a19846fef47
Ondřej Vít <ondra.vit@getfix.cz>
2022-05-02 17:50:26 +0200

    initial setup of react-router-dom

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,24 +2,18 @@ import React, { useEffect} from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../feature/activities/dashboard/ActivityDashboard';
-import Loader from './Loader';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../feature/home/HomePage';
 
 function App() {
-    const { activityStore } = useStore();
-
-    useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore]);
-
-    if (activityStore.loadingInitial) return <Loader content='Loading app' />
-
     return (
         <>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-                <ActivityDashboard />
+                <Route exact path='/' component={HomePage} />
+                <Route path='/activities' component={ActivityDashboard} />
+                <Route path='/createActivity' component={HomePage} />
             </Container>
         </>
     );
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './app/layout/App';
 import './app/layout/styles.css';
 import { store, StoreContext } from './app/stores/store';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </StoreContext.Provider>,
     document.getElementById('root') as HTMLElement
 );
