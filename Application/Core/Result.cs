c64e65b1cf6206b5627fa11c6777620e5da69b63
Ondřej Vít <ondra.vit@getfix.cz>
2022-05-03 23:10:48 +0200

    minor fix

--- Application/Core/Result.cs
+++ Application/Core/Result.cs
@@ -6,7 +6,7 @@
         public T Value { get; set; }
         public string Error { get; set; }
 
-        public static Result<T> Success(T value) => new Result<T>() { IsSuccess = true, Value = value };
-        public static Result<T> Failure(string error) => new Result<T>() { IsSuccess = true, Error = error };
+        public static Result<T> Success(T value) => new() { IsSuccess = true, Value = value };
+        public static Result<T> Failure(string error) => new() { IsSuccess = false, Error = error };
     }
 }
