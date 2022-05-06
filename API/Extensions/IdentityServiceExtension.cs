d0395f056d4b0d4532124250aba70db917f61633
Ondřej Vít <ondra.vit@getfix.cz>
2022-05-06 11:27:37 +0200

    set identity services

---
+++ API/Extensions/IdentityServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtension
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddEntityFrameworkStores<DbContext>()
+                .AddUserManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,7 @@ namespace API
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddAplicationService(_config);
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
